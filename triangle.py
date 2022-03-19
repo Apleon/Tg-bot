cebe6634e2451e765790ec36631ba84d87294888
Apleon <sharapov.a.v.prog@gmail.com>
2022-03-19 09:50:47 +0300

    refactor: formulas for side_c, refactor elif statements lines: 35 - 41

--- triangle.py
+++ triangle.py
@@ -5,9 +5,10 @@
 
 from math import*
 class Triangle:
-    def __init__(self, side_a = 8 , side_b = 6, height = 0.48, n=(0,0)):
+    def __init__(self, side_a = 6 , side_b = 8, height = 5, n=(0,0)):
         self.side_a = side_a
         self.side_b = side_b
+        self.side_c = sqrt((self.side_a ** 2) + (self.side_b ** 2))
         self.height = height
         self.n = n
         post = list(n)
@@ -15,8 +16,7 @@ class Triangle:
         self.y = post[1]
 
     def perimeter(self):
-        side_c = sqrt(self.side_a + self.side_b)
-        perimeter = self.side_a + self.side_b + side_c
+        perimeter = self.side_a + self.side_b + self.side_c
         print(perimeter)
     
     def area(self):
@@ -24,48 +24,42 @@ class Triangle:
         print(area)
 
     def distance(self):
-        return (self.x **2 + self.y **2) **0.5
+        return (self.x ** 2 + self.y ** 2) ** 0.5
 
     def position(self):
         print("x: ", self.x,"y: ", self.y)
 
-    def movement(self,direction,move):
+    def movement(self, direction, move):
         x_d = self.x
         y_d = self.y
-        print("direction_list = [UP, DOWN, LEFT, RIGHT]")
-        if direction == "UP":
+        if direction == "UP" or direction == "up":
             y_d = self.y + move
-        elif direction == "DOWN":
+        elif direction == "DOWN" or direction == "down":
             y_d = self.y - move
-        elif direction == "RIGHT":
+        elif direction == "RIGHT" or direction == "right":
             x_d = self.x + move
-        elif direction == "LEFT":
+        elif direction == "LEFT" or direction == "left":
             x_d = self.x - move
         self.x = x_d
         self.y = y_d
         return 0
 
-direction_list = ["UP","DOWN","LEFT","RIGHT"]    
+direction_list = ["UP", "DOWN", "LEFT", "RIGHT", "up", "down", "right", "left"]    
 triangle = Triangle()
-
-while True:
-    question = input('Do you want to enter a move? (YES/NO)\n')
-    while question == 'YES' or question == "Yes" or question == "yes":
-        a = input('Input DIRECTION AND DISTANCE\n').split()
-        if a[0] in direction_list:
-            #direction = a[0]
-            #distance = a[1]
-            triangle.movement(a[0], float(a[1]))
-            print("Position: ")
-            triangle.position()
-            print ("distance ",triangle.distance())
-        else: 
-            print('Invalid Input')  
-    break
-
 print("Perimeter: ")
 triangle.perimeter()
 print("Area: ")
 triangle.area()
-print("Movement: ")
-triangle.move()
+
+while True:
+    question = input('Do you want to enter a move? (YES/NO)\n')
+    while question == 'YES' or question == "Yes" or question == "yes":
+        a = input('INPUT DIRECTION AND DISTANCE\n').split()
+        if a[0] in direction_list:
+            triangle.movement(a[0], float(a[1]))
+            print("Position: ")
+            triangle.position()
+            print ("distance:",triangle.distance())
+        else: 
+            print('Invalid Input')  
+    break
